ffd2c839701fb10724abcf3cb61713c9762cb132
nieyim <minhtringuyenbt20@gmail.com>
2023-10-21 00:37:30 +0700

    Updated

--- api/server.js
+++ api/server.js
@@ -22,6 +22,7 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+server.use(jsonServer.bodyParser)
 
 server.use((req, res, next) => {
     if (req.method === 'POST') {
